798ae86776e26ba91769ba0fcc605dcf4562ab15
Victor <victorcyty@hotmail.com>
2024-02-07 20:02:35 -0300

    Changes in CorsRegistry

--- backend/src/main/java/com/vh/hms/cors/CorsConfiguration.java
+++ backend/src/main/java/com/vh/hms/cors/CorsConfiguration.java
@@ -10,6 +10,8 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
+                .allowedOrigins("http:localhost:3000")
+                .allowedOrigins("https://hospital-appointment-scheduling.vercel.app")
                 .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
                 .allowedHeaders("*")
